803e390b2be11c71aed910144a3df97aa2bad296
NIHAR KUMAR PATEL <niharkumarpatel01@gmail.com>
2026-09-11 01:09:01 +0530

    Updated Portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import viteReact from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tsConfigPaths(), tanstackStart(), viteReact(), tailwindcss()],
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart(),
+    netlify(),
+    viteReact(),
+    tailwindcss(),
+  ],
 });
